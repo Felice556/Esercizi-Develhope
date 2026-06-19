8fa7e4b639b01a112a723b15c30eccf43e9cffe2
Felice556 <felice.lgg@gmail.com>
2026-06-19 17:11:05 +0200

    operatore &&

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,14 @@
-
+import { Welcome } from "./Welcome";
+import { Age } from "./Age";
 
 export function Message() {
-    return <p> What a beautiful day! </p>
+    return (
+         <div>
+            {age > 18 ? (
+                <p>You are old enough!</p>
+            ) : (
+                <p>You are very young!</p>
+            )}
+        </div>
+    )
 }
--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,4 +1,5 @@
 import { Age } from "./Age";
+import { Message } from "./Message";
 export type WelcomeProps = {
     name?: string
     age?: number
@@ -10,6 +11,12 @@ export function Welcome({name = "World" , age = 0}: WelcomeProps) {
         <div>
             <p>Welcome, <strong>{name}</strong>!</p>
             <Age age={age} />
+            {age> 18 && <Age age={age} />}
+            {age && <Age age={age} />}
+            {age > 18 && age < 65 && <Age age={age} />}
+            {age > 18 && name === "John" && <Age age={age} />}
+            <Message age={age} />
+            
             
         </div>
     )
