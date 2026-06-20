af2a644372e8afd966c26e20fcbae5a68c3017bb
Felice556 <felice.lgg@gmail.com>
2026-06-20 21:01:02 +0200

    eventHandlers con buttons

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Counter } from './Counter'
 import ReactDOM from 'react-dom/client'
+import {MouseClicker} from './assets/MouseClicker'  
 
 
 const container = document.getElementById('root')!
-ReactDOM.createRoot(container).render(<Counter initialValue={0} increment={1} />)
+ReactDOM.createRoot(container).render(<MouseClicker />)
