91e1349df230efa9a0e14a78a0d86a09bd3f372e
Felice556 <felice.lgg@gmail.com>
2026-06-22 22:32:09 +0200

    react query libreria

--- src/useGithubUser.ts
+++ src/useGithubUser.ts
@@ -1,25 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+
 export function useGithubUser(username: string) {
-    const [user, setUser] = useState<any>(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<any>(null)
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['githubUser', username],
+        queryFn: () => fetch(`https://api.github.com/users/${username}`)
+            .then(res => res.json()),
+              enabled: username !== null 
+    })
 
-    const fetchUser = () => {        // 👈 funzione separata
-        setLoading(true)
-        fetch(`https://api.github.com/users/${username}`)
-            .then(res => res.json())
-            .then(data => {
-                setUser(data)
-                setLoading(false)
-            })
-            .catch(err => {
-                setError(err)
-                setLoading(false)
-            })
-    }
-
-    useEffect(() => {
-        fetchUser()                  
-    }, [username])  
-
-    return { user, loading, error, fetchUser }  
+    return { user: data, loading: isLoading, error }
 }
